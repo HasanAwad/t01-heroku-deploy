a0c072ade438341757b3ff8a47f01bd6fd230629
Codi Mac <codimac@Codis-MacBook-Pro.local>
2021-05-21 12:18:59 +0300

    try to fix the port problem

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
