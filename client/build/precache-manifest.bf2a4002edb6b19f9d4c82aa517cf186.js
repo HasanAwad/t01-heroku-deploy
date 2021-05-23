69d70ab4073f05bd05838453cb713f93158d9c9e
Codi Mac <codimac@Codis-MacBook-Pro.local>
2021-05-23 23:50:17 +0300

    update

---
+++ client/build/precache-manifest.bf2a4002edb6b19f9d4c82aa517cf186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e2d5934352bdd207e3fec8083b488a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ddd75ffa5d055bd33e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "91b0331cf9bfd519ef46",
+    "url": "/static/css/main.bcd68488.chunk.css"
+  },
+  {
+    "revision": "f8ddd75ffa5d055bd33e",
+    "url": "/static/js/2.b11e0292.chunk.js"
+  },
+  {
+    "revision": "b28a47af2e48da47b084c56123ff1325",
+    "url": "/static/js/2.b11e0292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91b0331cf9bfd519ef46",
+    "url": "/static/js/main.0cbc3254.chunk.js"
+  },
+  {
+    "revision": "f9cc1e1ff69344c38427",
+    "url": "/static/js/runtime-main.97c0d29c.js"
+  }
+]);
